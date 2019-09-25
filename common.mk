5e2a45b0d17b069622f14318615e5c703898fb13
Ashwin R C <ashwin2001achu@gmail.com>
2019-09-25 07:36:22 +0000

    sm8150-common: add back sensor hal

--- common.mk
+++ common.mk
@@ -400,8 +400,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@1.0-impl.oneplus_msmnile \
+    android.hardware.sensors@1.0-service.oneplus_msmnile \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
